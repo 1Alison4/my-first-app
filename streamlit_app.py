d77eb30aec881f3bf5cd118a0caa44c6cf842710
1Alison4 <techkenstein@gmail.com>
2025-02-27 02:57:49 +0000

    Mis cambios

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.title('My first Streamlit App')
+st.subheader('This is a subheader')
+st.subheader("Is this different", divider="violet")
+st.header('This is a header')
+st.text('And this is a text')
